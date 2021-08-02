53b58f4de52941ddbc7b662461230e22efb9a8a7
Eric Perreault <erperreault@gmail.com>
2021-08-01 22:24:08 -0400

    ditto

--- puzzles/BurrowsWheeler.py
+++ puzzles/BurrowsWheeler.py
@@ -10,6 +10,12 @@ class Solution:
         return [t, i]
 
     def decode(self, s, i):
+        """
+        this is the fancy trick: you can rebuild the original by tacking the encoded string onto
+        the fronts of each row and sorting alphabetically each time. answer is at the same index
+        after completing the table. table could be skipped by just sorting what you have and
+        checking the index each time.
+        """
         table = sorted([s[i] for i in range(len(s))])
         for x in range(len(s)-1):
             for y in range(len(s)):
