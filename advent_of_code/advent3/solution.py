1747e1a421b2fbef29227e056fd5a8cc33b28a36
Eric Perreault <erperreault@gmail.com>
2021-08-04 07:48:14 -0400

    advent3a done

--- advent_of_code/advent3/solution.py
+++ advent_of_code/advent3/solution.py
@@ -1,7 +1,17 @@
 from test import data
 
 def solution_a(data):
-    return 0
+    d = data.split()
+    coords = [0,0]
+    trees = 0
+
+    while coords[0] < len(d)-1:
+        coords[0] += 1
+        coords[1] = (coords[1] + 3) % len(d[0])
+        if d[coords[0]][coords[1]] == '#':
+            trees += 1
+
+    return trees
 
 def solution_b(data):
     return 0
