71f21321841346e08a740819a604728e5c9b0078
Eric Perreault <erperreault@gmail.com>
2021-08-04 07:54:56 -0400

    advent3b done

--- advent_of_code/advent3/solution.py
+++ advent_of_code/advent3/solution.py
@@ -13,8 +13,31 @@ def solution_a(data):
 
     return trees
 
-def solution_b(data):
-    return 0
+def solution_b(data):    
+    d = data.split()
+
+    ans = 1
+
+    slopes = [
+        [1,1], # [x,y]
+        [3,1],
+        [5,1],
+        [7,1],
+        [1,2],
+    ]
+
+    for slope in slopes:
+        trees = 0
+        coords = [0,0]
+
+        while coords[0] < len(d)-1:
+            coords[0] += slope[1] # y change
+            coords[1] = (coords[1] + slope[0]) % len(d[0]) # x change and wrap
+            if d[coords[0]][coords[1]] == '#':
+                trees += 1
+
+        ans *= trees
+    return ans
 
 
 print(solution_a(data))
