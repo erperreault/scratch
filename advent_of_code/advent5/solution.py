8b6f8e3ee16a75e3558efd03474decf11c9a6455
Eric Perreault <erperreault@gmail.com>
2021-08-05 11:23:10 -0400

    advent5b done

--- advent_of_code/advent5/solution.py
+++ advent_of_code/advent5/solution.py
@@ -5,12 +5,11 @@ def solution_a(data):
     highest = 0
 
     for seat in d:
-        print(seat)
         col = 7
         col_step = 4
         row = 127
         row_step = 64
-        
+
         for letter in seat[:7]:
             if letter == 'F':
                 row -= row_step
@@ -20,7 +19,6 @@ def solution_a(data):
             if letter == 'L':
                 col -= col_step
             col_step /= 2
-        print(f'{row=}, {col=}')
         
         seat_num = int(row*8 + col)
         if seat_num > highest:
@@ -29,7 +27,31 @@ def solution_a(data):
     return highest
 
 def solution_b(data):
-    pass
+    d = data.split()
+    seats = []
+
+    for seat in d:
+        col = 7
+        col_step = 4
+        row = 127
+        row_step = 64
+
+        for letter in seat[:7]:
+            if letter == 'F':
+                row -= row_step
+            row_step /= 2
+
+        for letter in seat[7:]:
+            if letter == 'L':
+                col -= col_step
+            col_step /= 2
+        
+        seat_num = int(row*8 + col)
+        seats.append(seat_num)
+
+    for i in range(max(seats)):
+        if i not in seats and i+1 in seats and i-1 in seats:
+            return i
 
 print(solution_a(data))
 print(solution_b(data))
