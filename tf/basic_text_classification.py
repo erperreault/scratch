ac2a98bd740cff81042aa2e6695ebaf1b664897e
Eric Perreault <erperreault@gmail.com>
2021-08-09 15:29:36 -0400

    start basic text classification

---
+++ tf/basic_text_classification.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import preprocessing
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+"""Pull the dataset of 50,000 reviews from IMDB.
+"""
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+dataset = tf.keras.utils.get_file("aclImdb_v1", url,
+                                    untar=True, cache_dir='.',
+                                    cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
