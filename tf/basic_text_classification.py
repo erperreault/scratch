248d8925af5ee5777c3a173f56b18b5df1895343
Eric Perreault <erperreault@gmail.com>
2021-08-09 16:19:04 -0400

    text preprocessing

--- tf/basic_text_classification.py
+++ tf/basic_text_classification.py
@@ -10,8 +10,7 @@ from tensorflow.keras import losses
 from tensorflow.keras import preprocessing
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
-"""Pull the dataset of 50,000 reviews from IMDB.
-"""
+"""Pull the dataset of 50,000 reviews from IMDB."""
 url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
 dataset = tf.keras.utils.get_file("aclImdb_v1", url,
@@ -19,3 +18,62 @@ dataset = tf.keras.utils.get_file("aclImdb_v1", url,
                                     cache_subdir='')
 
 dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+"""Remove unneeded dir from IMDB dataset."""
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+"""Create training dataset by 80/20 split from raw data."""
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', 
+    batch_size=batch_size, 
+    validation_split=0.2, 
+    subset='training', 
+    seed=seed)
+
+"""See some samples.
+for text_batch, label_batch in raw_train_ds.take(1):
+  for i in range(3):
+    print("Review", text_batch.numpy()[i])
+    print("Label", label_batch.numpy()[i])
+"""
+
+"""Create validation dataset by splitting from training."""
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train', 
+    batch_size=batch_size, 
+    validation_split=0.2, 
+    subset='validation', 
+    seed=seed)
+
+"""Create test dataset with remaining samples."""
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/test', 
+    batch_size=batch_size)
+
+"""Preprocess the text data to remove punctuation and HTML relics."""
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation),
+                                  '')
+
+"""Vectorize and standardize the text using the above method."""
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+"""Make a text-only dataset (without labels), then call adapt."""
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
