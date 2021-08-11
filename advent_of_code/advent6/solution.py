47b61c0808f409f6cfc94d4a7e481bcf8e796668
Eric Perreault <erperreault@gmail.com>
2021-08-11 09:01:02 -0400

    refactor 6a

--- advent_of_code/advent6/solution.py
+++ advent_of_code/advent6/solution.py
@@ -2,9 +2,9 @@ from test import data
 
 def solution_a(data):
     """Harder to read one-liner."""
-    return sum([ len(set(x.replace('\n', ''))) for x in data.split('\n\n') ])
+    #return sum([ len(set(group.replace('\n', ''))) for group in data.split('\n\n') ])
 
-    formatted = [ ''.join(group.split('\n')) for group in data.split('\n\n') ]
+    formatted = [ group.replace('\n', '') for group in data.split('\n\n') ]
     return sum([ len(set(group)) for group in formatted ])
 
 def solution_b(data):
