06c190644b2b903d391af5c9b027059845f87d33
Eric Perreault <erperreault@gmail.com>
2021-08-14 14:03:55 -0400

    ditto

--- ds&a exercises/BinarySearch/solution.py
+++ ds&a exercises/BinarySearch/solution.py
@@ -1,8 +1,12 @@
 """Implement binary search of a sorted array of integers.
 Return the index of the target integer if it is present, or else -1 if it is absent.
+
+Variant: 
+Use binary search to find the first number greater than a cutoff value (given as 'target').
 """
 
 class Solution:
+    """Solve for base case."""
     def solution(self, nums: list[int], target: int) -> int:
         low = 0
         high = len(nums)-1
@@ -20,3 +24,7 @@ class Solution:
                     high = mid - 1
                     
         return -1
+
+    """Variant solution."""
+    def v_solution(self):
+        pass
