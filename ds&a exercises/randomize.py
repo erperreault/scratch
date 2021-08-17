b195b787968671f68fffd3419aaa24ab5f6180ef
Eric Perreault <erperreault@gmail.com>
2021-08-17 10:10:08 -0400

    ditto

--- ds&a exercises/randomize.py
+++ ds&a exercises/randomize.py
@@ -2,15 +2,19 @@ import random
 
 exercises = {
     'Linked List':  '''Implement a linked list.
-Define a Node class, and populate a linked list of Nodes. Write a second method, which reads the values of the linked list and returns them as an array.''',
+Define a Node class, and populate a linked list of Nodes. 
+Write a second method, which reads the values of the linked list and returns them as an array.''',
     'Breadth First':  '''Implement breadth-first search on a binary tree.
 Variant: return the level-order traversal of the full tree as an array of arrays.''',
+    'Depth First':  '''Implement depth-first search on a binary tree.
+Variant: return the left-right traversal of the full tree as an array of arrays.''',
     'Merge Sort':   '''Implement merge sort on an array of integers.
 Variant: implement alphabetical merge sort on an array of strings''',
     'Binary Search': '''Implement binary search of a sorted array of integers.
 Return the index of the target integer if it is present, or else -1 if it is absent.
 Variant: use binary search to find the first number greater than a cutoff value (given as 'target').''',
-    'Binary Tree': '''Implement a binary tree data structure. Given an array (with nulls for non-existent nodes), populate the tree.'''
+    'Binary Tree': '''Implement a binary tree data structure. Given an array (with nulls for non-existent nodes), populate the tree.''',
+    'Balanced Binary Tree': '''Given a binary tree, determine if it is balanced.''',
 }
 
 
