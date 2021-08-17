1c5822e94547ffbebd9922642ea3cebb1323d00d
Eric Perreault <erperreault@gmail.com>
2021-08-17 10:02:50 -0400

    flesh out prompts for randomize

--- ds&a exercises/randomize.py
+++ ds&a exercises/randomize.py
@@ -2,22 +2,20 @@ import random
 
 exercises = {
     'Linked List':  '''Implement a linked list.
-Define a Node class, and populate a linked list of Nodes with values counting up from
-the given positive start integer until double its value (exclusive).
-
-Write a second method, which reads the values of the linked list and returns them as an array.
-
-Integers less than 1 should return an empty array.''',
-    'Linked List':  '''Implement a linked list.
-Define a Node class, and populate a linked list of Nodes with values counting up from
-the given positive start integer until double its value (exclusive).
-
-Write a second method, which reads the values of the linked list and returns them as an array.
-
-Integers less than 1 should return an empty array.''',
+Define a Node class, and populate a linked list of Nodes. Write a second method, which reads the values of the linked list and returns them as an array.''',
+    'Breadth First':  '''Implement breadth-first search on a binary tree.
+Variant: return the level-order traversal of the full tree as an array of arrays.''',
+    'Merge Sort':   '''Implement merge sort on an array of integers.
+Variant: implement alphabetical merge sort on an array of strings''',
+    'Binary Search': '''Implement binary search of a sorted array of integers.
+Return the index of the target integer if it is present, or else -1 if it is absent.
+Variant: use binary search to find the first number greater than a cutoff value (given as 'target').''',
+    'Binary Tree': '''Implement a binary tree data structure. Given an array (with nulls for non-existent nodes), populate the tree.'''
 }
 
 
 if __name__ == '__main__':
     selection = random.choice(list(exercises.keys()))
-    print(f'\n{selection}\n\n{exercises[selection]}\n')
+    print('//////////\n')
+    print(f'{selection}\n\n{exercises[selection]}')
+    print('\n//////////')
