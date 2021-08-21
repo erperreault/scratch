16f47d02ff80438fcc0e4935c7bc95c6d10d7e74
Eric Perreault <erperreault@gmail.com>
2021-08-21 10:59:55 -0400

    ditto

--- advent_of_code/advent7/solution.py
+++ advent_of_code/advent7/solution.py
@@ -1,4 +1,10 @@
-class Solution:
+from test import data
 
-    def solution(self):
-        return 0
+def solution_a(data):
+    pass
+
+def solution_b(data):
+    pass
+
+print(solution_a(data))
+print(solution_b(data))
