073586070b5108f701b895b575720a5ee7855504
Eric Perreault <erperreault@gmail.com>
2021-08-25 15:50:06 -0400

    setup quick sort

--- ds&a exercises/SortQuicksort/solution.py
+++ ds&a exercises/SortQuicksort/solution.py
@@ -1,9 +1,19 @@
 class Solution:
 
     def solution(self, a: list) -> list:
-        pivot = len(a)-1
-        i = 0
-        j = 1
+        """This solution does not sort in place but is very easy to understand."""
+        if len(a) < 2:
+            return a
 
-        while a[i] < a[pivot]:
+        less, equal, greater = [], [], []
+        pivot = a[0]
+
+        for i in a:
+            if i < pivot:
+                less.append(i)
+            elif i == pivot:
+                equal.append(i)
+            else:
+                greater.append(i)
             
+        return self.solution(less) + equal + self.solution(greater)
