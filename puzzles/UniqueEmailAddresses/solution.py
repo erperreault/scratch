f9e5cba83ad3c0af5580f35b5bc5bd9e24a8e03c
Eric Perreault <erperreault@gmail.com>
2021-09-27 09:27:59 -0400

    finish unique emails

--- puzzles/UniqueEmailAddresses/solution.py
+++ puzzles/UniqueEmailAddresses/solution.py
@@ -4,22 +4,14 @@ class Solution:
 
     def solution(self, emails: list) -> int:
         unique = []
-
+        
         for e in emails:
-            if '@' not in e or '.' not in e:
-                continue
             e = e.split('@')
-            if len(e) != 2 or '.' not in e[1]:
-                continue
             if '+' in e[0]:
                 e[0] = e[0].split('+')[0]
             if '.' in e[0]:
                 e[0] = ''.join(e[0].split('.'))
-            e = ''.join(e)
-            print(e)
-            if e in unique:
-                continue
+            e = '@'.join(e)
             unique.append(e)
-
-
-        return len(unique)
+            
+        return len(set(unique))
